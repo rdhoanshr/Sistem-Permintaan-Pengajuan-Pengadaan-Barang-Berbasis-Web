bb666ff8fb1cbaaad4a4cf2d598617853c7dd51a
rdhoanshr <anshori706@gmail.com>
2023-09-04 07:53:35 +0800

    Update fitur data barang - tambah barang

---
+++ application/controllers/Barang.php
@@ -0,0 +1,204 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Barang extends CI_Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->library(['ion_auth', 'form_validation']);
+
+        if (!$this->ion_auth->logged_in()) {
+            redirect('auth/login', 'refresh');
+        } else if ($this->ion_auth->is_admin() || $this->ion_auth->in_group('kabag')) {
+        } else {
+            show_error('You must be an administrator to view this page.');
+        }
+
+        $this->load->model('BarangModel');
+    }
+
+
+    public function index()
+    {
+        $data['title'] = 'Data Barang';
+        $data['barang'] = $this->BarangModel->lihat();
+
+        $this->load->view('barang/data_barang', $data);
+    }
+
+    public function tambah()
+    {
+        $data['title'] = 'Tambah Barang';
+
+        $this->form_validation->set_rules('nama_barang', 'Nama Barang', 'required');
+        $this->form_validation->set_rules('jenis_barang', 'Jenis Barang', 'required');
+        $this->form_validation->set_rules('satuan', 'Satuan', 'required');
+        $this->form_validation->set_rules('keterangan', 'Keterangan', 'required');
+
+        if ($this->form_validation->run() == false) {
+            $this->load->view('barang/tambah_barang', $data);
+        } else {
+            $this->BarangModel->proses_tambah();
+            $err = $this->db->error();
+            if ($err['code'] !== 0) {
+                echo $err['message'];
+            } else {
+                $this->session->set_flashdata('pesanbaik', 'Barang Berhasil Di tambah');
+                redirect('barang');
+            }
+        }
+    }
+
+    // public function edit($id)
+    // {
+    //     $data['title'] = 'SIPB Kertak Hanyar | Edit Penduduk';
+    //     $data['sesi_user'] = $this->ion_auth->user()->row();
+    //     $data['penduduk'] = $this->PendudukModel->getPenduduk($id);
+    //     $data['agama'] = $this->PendudukModel->agama();
+    //     $data['pendidikan'] = $this->PendudukModel->pendidikan();
+    //     $data['status_kawin'] = $this->PendudukModel->status_kawin();
+
+    //     $data['akses'] = 'admin';
+
+    //     $this->form_validation->set_rules('nik', 'NIK', 'required');
+    //     $this->form_validation->set_rules('no_kk', 'No. Kartu Keluarga', 'required');
+    //     $this->form_validation->set_rules('nama_penduduk', 'Nama Penduduk', 'required');
+    //     $this->form_validation->set_rules('tempat_lahir', 'Tempat Lahir', 'required');
+    //     $this->form_validation->set_rules('tgl_lahir', 'Tanggal Lahir', 'required');
+    //     $this->form_validation->set_rules('alamat', 'Alamat', 'required');
+    //     $this->form_validation->set_rules('pekerjaan', 'Pekerjaan', 'required');
+    //     $this->form_validation->set_rules('no_rt', 'No. RT', 'required');
+    //     $this->form_validation->set_rules('no_rw', 'No. RW', 'required');
+    //     $this->form_validation->set_rules('nm_ayah', 'Nama Ayah', 'required');
+    //     $this->form_validation->set_rules('nm_ibu', 'Nama Ibu', 'required');
+    //     $this->form_validation->set_rules('hubungan_keluarga', 'Status Hubungan Dalam Keluarga', 'required');
+
+    //     if ($this->form_validation->run() == false) {
+    //         $this->load->view('admin/penduduk/edit', $data);
+    //     } else {
+    //         $this->PendudukModel->proses_edit($id);
+    //         $err = $this->db->error();
+    //         if ($err['code'] !== 0) {
+    //             echo $err['message'];
+    //         } else {
+    //             $this->session->set_flashdata('message', 'Penduduk Berhasil Di update');
+    //             redirect('admin/penduduk');
+    //         }
+    //     }
+    // }
+    // public function hapus($id)
+    // {
+    //     $this->PendudukModel->hapus($id);
+    //     $this->session->set_flashdata('message', 'Penduduk Berhasil Di Hapus');
+    //     redirect('admin/penduduk');
+    // }
+
+    // public function dataLapor()
+    // {
+    //     $data['title'] = 'SIPB Kertak Hanyar | Data Permohonan Ubah Data Penduduk';
+    //     $data['sesi_user'] = $this->ion_auth->user()->row();
+    //     $data['lapor'] = $this->PendudukModel->data_lapor();
+    //     $data['akses'] = 'admin';
+
+    //     $this->load->view('admin/penduduk/dataLapor', $data);
+    // }
+
+    // public function dataVerif()
+    // {
+    //     $data['title'] = 'SIPB Kertak Hanyar | Data Permohonan Ubah Data Penduduk';
+    //     $data['sesi_user'] = $this->ion_auth->user()->row();
+    //     $data['lapor'] = $this->PendudukModel->data_verif();
+    //     $data['akses'] = 'admin';
+
+    //     $this->load->view('admin/penduduk/dataVerif', $data);
+    // }
+
+    // public function ubahLapor($nik)
+    // {
+    //     $data['title'] = 'SIPB Kertak Hanyar | Edit Penduduk';
+    //     $data['sesi_user'] = $this->ion_auth->user()->row();
+    //     $data['penduduk'] = $this->PendudukModel->getlapor($nik);
+    //     $data['agama'] = $this->PendudukModel->agama();
+    //     $data['pendidikan'] = $this->PendudukModel->pendidikan();
+    //     $data['status_kawin'] = $this->PendudukModel->status_kawin();
+
+    //     $data['akses'] = 'admin';
+
+    //     $this->form_validation->set_rules('nik', 'NIK', 'required');
+    //     $this->form_validation->set_rules('no_kk', 'No. Kartu Keluarga', 'required');
+    //     $this->form_validation->set_rules('nama_penduduk', 'Nama Penduduk', 'required');
+    //     $this->form_validation->set_rules('tempat_lahir', 'Tempat Lahir', 'required');
+    //     $this->form_validation->set_rules('tgl_lahir', 'Tanggal Lahir', 'required');
+    //     $this->form_validation->set_rules('alamat', 'Alamat', 'required');
+    //     $this->form_validation->set_rules('pekerjaan', 'Pekerjaan', 'required');
+    //     $this->form_validation->set_rules('no_rt', 'No. RT', 'required');
+    //     $this->form_validation->set_rules('no_rw', 'No. RW', 'required');
+    //     $this->form_validation->set_rules('nm_ayah', 'Nama Ayah', 'required');
+    //     $this->form_validation->set_rules('nm_ibu', 'Nama Ibu', 'required');
+    //     $this->form_validation->set_rules('hubungan_keluarga', 'Status Hubungan Dalam Keluarga', 'required');
+
+    //     if ($this->form_validation->run() == false) {
+    //         $this->load->view('admin/penduduk/ubahLapor', $data);
+    //     } else {
+    //         $this->PendudukModel->proses_ubahLapor($nik);
+    //         $err = $this->db->error();
+    //         if ($err['code'] !== 0) {
+    //             echo $err['message'];
+    //         } else {
+    //             $this->session->set_flashdata('message', 'Permohonan Selesai');
+    //             redirect('admin/penduduk/dataLapor');
+    //         }
+    //     }
+    // }
+
+    // public function Verify($id)
+    // {
+    //     $data['title'] = 'SIPB Kertak Hanyar | Verifikasi Penduduk';
+    //     $data['sesi_user'] = $this->ion_auth->user()->row();
+    //     $data['penduduk'] = $this->PendudukModel->get_statusverif($id);
+    //     $data['agama'] = $this->PendudukModel->agama();
+    //     $data['pendidikan'] = $this->PendudukModel->pendidikan();
+    //     $data['status_kawin'] = $this->PendudukModel->status_kawin();
+
+    //     $data['akses'] = 'admin';
+
+    //     $this->form_validation->set_rules('nik', 'NIK', 'required');
+    //     $this->form_validation->set_rules('no_kk', 'No. Kartu Keluarga', 'required');
+    //     $this->form_validation->set_rules('nama_penduduk', 'Nama Penduduk', 'required');
+    //     $this->form_validation->set_rules('tempat_lahir', 'Tempat Lahir', 'required');
+    //     $this->form_validation->set_rules('tgl_lahir', 'Tanggal Lahir', 'required');
+    //     $this->form_validation->set_rules('alamat', 'Alamat', 'required');
+    //     $this->form_validation->set_rules('pekerjaan', 'Pekerjaan', 'required');
+    //     $this->form_validation->set_rules('no_rt', 'No. RT', 'required');
+    //     $this->form_validation->set_rules('no_rw', 'No. RW', 'required');
+    //     $this->form_validation->set_rules('nm_ayah', 'Nama Ayah', 'required');
+    //     $this->form_validation->set_rules('nm_ibu', 'Nama Ibu', 'required');
+    //     $this->form_validation->set_rules('hubungan_keluarga', 'Status Hubungan Dalam Keluarga', 'required');
+
+    //     if ($this->form_validation->run() == false) {
+    //         $this->load->view('admin/penduduk/verif', $data);
+    //     } else {
+    //         $this->PendudukModel->verifikasi($id);
+    //         $err = $this->db->error();
+    //         if ($err['code'] !== 0) {
+    //             echo $err['message'];
+    //         } else {
+    //             $this->session->set_flashdata('message', 'Permohonan Selesai');
+    //             redirect('admin/penduduk/dataVerif');
+    //         }
+    //     }
+    // }
+
+    // public function tolakVerifikasi($id)
+    // {
+    //     $this->PendudukModel->tolakVerifikasi($id);
+    //     $err = $this->db->error();
+    //     if ($err['code'] !== 0) {
+    //         echo $err['message'];
+    //     } else {
+    //         $this->session->set_flashdata('message', 'Permohonan SPP Berhasil Di Tolak');
+    //         redirect('admin/penduduk/dataverif');
+    //     }
+    // }
+}
---
+++ application/views/barang/data_barang.php
@@ -0,0 +1,93 @@
+<!-- Memanggil file header.php -->
+<?php $this->load->view("layout_backoffice/header") ?>
+
+<!-- Memanggil file navbar.php -->
+<?php $this->load->view("layout_backoffice/navbar") ?>
+
+<!-- Memanggil file sidebar.php -->
+<?php $this->load->view("layout_backoffice/sidebar") ?>
+
+
+
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
+    <!-- Menampilkan notif flashdata -->
+    <?php if ($this->session->flashdata('pesanbaik')) : ?>
+        <div class="alert alert-success" role="alert">
+            <?php echo $this->session->flashdata('pesanbaik'); ?>
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
+        </div>
+    <?php endif; ?>
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+        <h1>
+            Data Barang
+        </h1>
+        <ol class="breadcrumb">
+            <li><a href="<?= base_url(); ?>"><i class="fa fa-dashboard"></i> Home</a></li>
+            <li class="active">Data Barang</li>
+        </ol>
+    </section>
+
+    <!-- Main content -->
+    <section class="content">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-12">
+                    <!-- /.box -->
+                    <div class="box">
+                        <div class="box-header">
+                            <a href="<?= base_url('barang/tambah'); ?>" class="btn btn-primary"><i class="fa fa-plus"></i> Tambah</a>
+                        </div>
+                        <!-- /.box-header -->
+                        <div class="box-body">
+                            <div class="table-responsive">
+                                <table id="example1" class="table table-bordered table-striped">
+                                    <thead>
+                                        <tr>
+                                            <th>No</th>
+                                            <th>Nama</th>
+                                            <th>Jenis</th>
+                                            <th>Satuan</th>
+                                            <th>Keterangan</th>
+                                            <th>Aksi</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <?php
+                                        $i = 1;
+                                        foreach ($barang as $b) : ?>
+                                            <tr>
+                                                <td><?= $i++; ?></td>
+                                                <td><?= $b['nama_barang']; ?>></td>
+                                                <td><?= $b['jenis_barang']; ?></td>
+                                                <td><?= $b['satuan']; ?> </td>
+                                                <td><?= $b['keterangan']; ?> </td>
+                                                <td>
+                                                    <a href="<?= base_url('barang/edit/') . $b['id_barang']; ?>" class="btn btn-sm btn-warning"><i class="fa fa-edit"></i></a>
+                                                    <a href="<?= base_url('barang/hapus/') . $b['id_barang']; ?>" class="btn btn-sm btn-danger" onclick="return confirm('Apakah Anda Yakin Menghapus Barang ini ?')"><i class="fa fa-trash"></i></a>
+                                                </td>
+                                            </tr>
+                                        <?php endforeach; ?>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                        <!-- /.card-body -->
+                    </div>
+                    <!-- /.card -->
+                </div>
+                <!-- /.col -->
+            </div>
+            <!-- /.row -->
+        </div>
+        <!-- /.container-fluid -->
+    </section>
+    <!-- /.content -->
+</div>
+<!-- /.content-wrapper -->
+
+<!-- Memanggil file footer.php -->
+<?php $this->load->view("layout_backoffice/footer") ?>
