943849102eb87a15565d2dc846cee9c286ff5f89
rdhoanshr <anshori706@gmail.com>
2023-09-04 13:41:01 +0800

    Perbaikan fitur tambah unit

--- application/views/unit/tambah_unit.php
+++ application/views/unit/tambah_unit.php
@@ -25,7 +25,7 @@
     <!-- Content Header (Page header) -->
     <section class="content-header">
         <h1>
-            Tambah Barang
+            Tambah Unit
         </h1>
         <ol class="breadcrumb">
             <li><a href="<?= base_url(); ?>"><i class="fa fa-dashboard"></i> Home</a></li>
