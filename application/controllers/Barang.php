cb9723b5ce62a020c561ade5e8460eab617326bc
rdhoanshr <anshori706@gmail.com>
2023-09-04 08:14:11 +0800

    Update fitur data barang - hapus barang

--- application/controllers/Barang.php
+++ application/controllers/Barang.php
@@ -73,118 +73,16 @@ class Barang extends CI_Controller
             }
         }
     }
-    // public function hapus($id)
-    // {
-    //     $this->PendudukModel->hapus($id);
-    //     $this->session->set_flashdata('message', 'Penduduk Berhasil Di Hapus');
-    //     redirect('admin/penduduk');
-    // }
 
-    // public function dataLapor()
-    // {
-    //     $data['title'] = 'SIPB Kertak Hanyar | Data Permohonan Ubah Data Penduduk';
-    //     $data['sesi_user'] = $this->ion_auth->user()->row();
-    //     $data['lapor'] = $this->PendudukModel->data_lapor();
-    //     $data['akses'] = 'admin';
-
-    //     $this->load->view('admin/penduduk/dataLapor', $data);
-    // }
-
-    // public function dataVerif()
-    // {
-    //     $data['title'] = 'SIPB Kertak Hanyar | Data Permohonan Ubah Data Penduduk';
-    //     $data['sesi_user'] = $this->ion_auth->user()->row();
-    //     $data['lapor'] = $this->PendudukModel->data_verif();
-    //     $data['akses'] = 'admin';
-
-    //     $this->load->view('admin/penduduk/dataVerif', $data);
-    // }
-
-    // public function ubahLapor($nik)
-    // {
-    //     $data['title'] = 'SIPB Kertak Hanyar | Edit Penduduk';
-    //     $data['sesi_user'] = $this->ion_auth->user()->row();
-    //     $data['penduduk'] = $this->PendudukModel->getlapor($nik);
-    //     $data['agama'] = $this->PendudukModel->agama();
-    //     $data['pendidikan'] = $this->PendudukModel->pendidikan();
-    //     $data['status_kawin'] = $this->PendudukModel->status_kawin();
-
-    //     $data['akses'] = 'admin';
-
-    //     $this->form_validation->set_rules('nik', 'NIK', 'required');
-    //     $this->form_validation->set_rules('no_kk', 'No. Kartu Keluarga', 'required');
-    //     $this->form_validation->set_rules('nama_penduduk', 'Nama Penduduk', 'required');
-    //     $this->form_validation->set_rules('tempat_lahir', 'Tempat Lahir', 'required');
-    //     $this->form_validation->set_rules('tgl_lahir', 'Tanggal Lahir', 'required');
-    //     $this->form_validation->set_rules('alamat', 'Alamat', 'required');
-    //     $this->form_validation->set_rules('pekerjaan', 'Pekerjaan', 'required');
-    //     $this->form_validation->set_rules('no_rt', 'No. RT', 'required');
-    //     $this->form_validation->set_rules('no_rw', 'No. RW', 'required');
-    //     $this->form_validation->set_rules('nm_ayah', 'Nama Ayah', 'required');
-    //     $this->form_validation->set_rules('nm_ibu', 'Nama Ibu', 'required');
-    //     $this->form_validation->set_rules('hubungan_keluarga', 'Status Hubungan Dalam Keluarga', 'required');
-
-    //     if ($this->form_validation->run() == false) {
-    //         $this->load->view('admin/penduduk/ubahLapor', $data);
-    //     } else {
-    //         $this->PendudukModel->proses_ubahLapor($nik);
-    //         $err = $this->db->error();
-    //         if ($err['code'] !== 0) {
-    //             echo $err['message'];
-    //         } else {
-    //             $this->session->set_flashdata('message', 'Permohonan Selesai');
-    //             redirect('admin/penduduk/dataLapor');
-    //         }
-    //     }
-    // }
-
-    // public function Verify($id)
-    // {
-    //     $data['title'] = 'SIPB Kertak Hanyar | Verifikasi Penduduk';
-    //     $data['sesi_user'] = $this->ion_auth->user()->row();
-    //     $data['penduduk'] = $this->PendudukModel->get_statusverif($id);
-    //     $data['agama'] = $this->PendudukModel->agama();
-    //     $data['pendidikan'] = $this->PendudukModel->pendidikan();
-    //     $data['status_kawin'] = $this->PendudukModel->status_kawin();
-
-    //     $data['akses'] = 'admin';
-
-    //     $this->form_validation->set_rules('nik', 'NIK', 'required');
-    //     $this->form_validation->set_rules('no_kk', 'No. Kartu Keluarga', 'required');
-    //     $this->form_validation->set_rules('nama_penduduk', 'Nama Penduduk', 'required');
-    //     $this->form_validation->set_rules('tempat_lahir', 'Tempat Lahir', 'required');
-    //     $this->form_validation->set_rules('tgl_lahir', 'Tanggal Lahir', 'required');
-    //     $this->form_validation->set_rules('alamat', 'Alamat', 'required');
-    //     $this->form_validation->set_rules('pekerjaan', 'Pekerjaan', 'required');
-    //     $this->form_validation->set_rules('no_rt', 'No. RT', 'required');
-    //     $this->form_validation->set_rules('no_rw', 'No. RW', 'required');
-    //     $this->form_validation->set_rules('nm_ayah', 'Nama Ayah', 'required');
-    //     $this->form_validation->set_rules('nm_ibu', 'Nama Ibu', 'required');
-    //     $this->form_validation->set_rules('hubungan_keluarga', 'Status Hubungan Dalam Keluarga', 'required');
-
-    //     if ($this->form_validation->run() == false) {
-    //         $this->load->view('admin/penduduk/verif', $data);
-    //     } else {
-    //         $this->PendudukModel->verifikasi($id);
-    //         $err = $this->db->error();
-    //         if ($err['code'] !== 0) {
-    //             echo $err['message'];
-    //         } else {
-    //             $this->session->set_flashdata('message', 'Permohonan Selesai');
-    //             redirect('admin/penduduk/dataVerif');
-    //         }
-    //     }
-    // }
-
-    // public function tolakVerifikasi($id)
-    // {
-    //     $this->PendudukModel->tolakVerifikasi($id);
-    //     $err = $this->db->error();
-    //     if ($err['code'] !== 0) {
-    //         echo $err['message'];
-    //     } else {
-    //         $this->session->set_flashdata('message', 'Permohonan SPP Berhasil Di Tolak');
-    //         redirect('admin/penduduk/dataverif');
-    //     }
-    // }
+    public function hapus($id)
+    {
+        $this->BarangModel->hapus($id);
+        $err = $this->db->error();
+        if ($err['code'] !== 0) {
+            echo $err['message'];
+        } else {
+            $this->session->set_flashdata('pesanbaik', 'Barang Berhasil Di Hapus');
+            redirect('barang');
+        }
+    }
 }
